e3be0630957267aaac654bccb80218a4baa62fd3
gaiar <gaiar@baimuratov.ru>
2018-05-14 20:39:02 +0200

    fixes

--- recipe/ffmpeg-libraries-recipies/libnuma/recipe/build.sh
+++ recipe/ffmpeg-libraries-recipies/libnuma/recipe/build.sh
@@ -3,9 +3,7 @@ CFLAGS="-march=native -mtune=native"
 export CFLAGS
 export CXXFLAGS="${CFLAGS}"
 
-chmod +x autogen.sh
-
-./autogen.sh --prefix=${PREFIX}
+autoreconf -fiv
 
 make -j ${CPU_COUNT} 
 make install PREFIX=${PREFIX}
