fa117463e77d587738379faf782afcc654fa729e
gaiar <gaiar@baimuratov.ru>
2018-05-14 22:20:16 +0200

    fix

--- recipe/ffmpeg-libraries-recipies/libx265/recipe/build.sh
+++ recipe/ffmpeg-libraries-recipies/libx265/recipe/build.sh
@@ -4,7 +4,7 @@ mkdir -vp ${PREFIX}/bin
 
 #mkdir -vp build
 #cd build
-
-cmake -G "Unix Makefiles" -DENABLE_LIBNUMA=ON -DENABLE_PIC=ON -DNATIVE_BUILD=ON -DENABLE_SHARED=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX .
-#make -j ${CPU_COUNT} 
+cd build/arm-linux
+cmake -G "Unix Makefiles" -DENABLE_LIBNUMA=OFF -DENABLE_PIC=ON -DNATIVE_BUILD=ON -DENABLE_SHARED=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../../source
+make -j ${CPU_COUNT} 
 make install PREFIX=${PREFIX}
