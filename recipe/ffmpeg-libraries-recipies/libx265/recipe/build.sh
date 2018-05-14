c08c1da8474589d97abc02a8683f318fd24bfaaf
gaiar <gaiar@baimuratov.ru>
2018-05-14 20:50:35 +0200

    fix

--- recipe/ffmpeg-libraries-recipies/libx265/recipe/build.sh
+++ recipe/ffmpeg-libraries-recipies/libx265/recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mkdir -vp ${PREFIX}/bin
 
-cmake -G "Unix Makefiles" -DENABLE_LIBNUMA=ON -DENABLE_PIC=ON -DNATIVE_BUILD=ON -DENABLE_SHARED=ON ../source
-make -j ${CPU_COUNT} 
+
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" -DENABLE_LIBNUMA=ON -DENABLE_PIC=ON -DNATIVE_BUILD=ON -DENABLE_SHARED=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+#make -j ${CPU_COUNT} 
 make install PREFIX=${PREFIX}
